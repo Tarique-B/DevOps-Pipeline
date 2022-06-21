2a22d8e7751219b660053c046d4466e6e44a9d97
Ubuntu <ubuntu@ip-172-31-12-139.ap-south-1.compute.internal>
2022-06-21 16:13:18 +0000

    assignment files

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my-s3-bucket.id
+}
+
+output "s3_bucket_region" {
+    value = aws_s3_bucket.my-s3-bucket.region
+}
+output "lambda" {
+  value = "${aws_lambda_function.lambda.qualified_arn}"
+}
